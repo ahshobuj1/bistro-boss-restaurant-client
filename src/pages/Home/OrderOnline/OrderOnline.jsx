25b3901992e9e8fdc9979377094b5e7bdf69284f
A H Shobuj <ahshobuj09@gmail.com>
2024-09-13 17:19:51 +0600

    created Reusable section title components and completed Onine Order Section

--- src/pages/Home/OrderOnline/OrderOnline.jsx
+++ src/pages/Home/OrderOnline/OrderOnline.jsx
@@ -9,11 +9,14 @@ import img2 from '../../../assets/home/slide2.jpg';
 import img3 from '../../../assets/home/slide3.jpg';
 import img4 from '../../../assets/home/slide4.jpg';
 import img5 from '../../../assets/home/slide5.jpg';
+import SectionTitle from '../../Shared/SectionTitle/SectionTitle';
 
 const OrderOnline = () => {
     return (
-        <section className="py-24">
-            <div></div>
+        <section className="py-5 md:py-20">
+            <div>
+                <SectionTitle />
+            </div>
             <div>
                 <Swiper
                     slidesPerView={2}
@@ -39,18 +42,33 @@ const OrderOnline = () => {
                     className="mySwiper">
                     <SwiperSlide>
                         <img src={img1} alt="menu images" />
+                        <p className="uppercase absolute left-20 bottom-5 z-10 text-white">
+                            salads
+                        </p>
                     </SwiperSlide>
                     <SwiperSlide>
                         <img src={img2} alt="menu images" />
+                        <p className="uppercase absolute left-20 bottom-5 z-10 text-white">
+                            soups
+                        </p>
                     </SwiperSlide>
                     <SwiperSlide>
                         <img src={img3} alt="menu images" />
+                        <p className="uppercase absolute left-20 bottom-5 z-10 text-white">
+                            pizzas
+                        </p>
                     </SwiperSlide>
                     <SwiperSlide>
                         <img src={img4} alt="menu images" />
+                        <p className="uppercase absolute left-20 bottom-5 z-10 text-white">
+                            desserts
+                        </p>
                     </SwiperSlide>
                     <SwiperSlide>
                         <img src={img5} alt="menu images" />
+                        <p className="uppercase absolute left-20 bottom-5 z-10 text-white">
+                            salads
+                        </p>
                     </SwiperSlide>
                 </Swiper>
             </div>
---
+++ src/pages/Shared/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,12 @@
+const SectionTitle = () => {
+    return (
+        <div className="max-w-md mx-auto text-center mb-6 md:mb-12 px-5 md:px-0">
+            <p className="text-[#d99904] pb-4">---From 11:00am to 10:00pm---</p>
+            <h2 className="uppercase text-3xl md:text-4xl py-5 border-y-4">
+                ORDER ONLINE
+            </h2>
+        </div>
+    );
+};
+
+export default SectionTitle;
