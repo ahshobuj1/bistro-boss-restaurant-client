8af214fadf089df27aef0f7d7a2c07bee8be6417
A H Shobuj <ahshobuj09@gmail.com>
2024-09-17 01:38:44 +0600

    Control form with react-hook-forms package , Get value, Handle Errors and display errors with easiest ways, Control validation and many more

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,60 +2,113 @@ import {Link} from 'react-router-dom';
 import bgImage from '../../assets/others/authentication.png';
 import SignUpImg from '../../assets/others/authentication2.png';
 import {FaFacebook, FaGithub, FaGoogle} from 'react-icons/fa';
+import {useForm} from 'react-hook-form';
 
 const SignUp = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+        const {name, photoURL, email, password} = data;
+    };
+
+    console.log('found errors ', errors);
+
     return (
         <section>
             <div className="hero bg-transparent shadow-2xl">
-                <img src={bgImage} alt="" className="w-full h-full" />
+                <img
+                    src={bgImage}
+                    alt="background image"
+                    className="w-full h-full"
+                />
                 <div className="hero-content flex-col lg:flex-row-reverse mx-5 my-24 shadow-2xl rounded-xl">
                     <div className="text-center lg:text-left">
                         <img src={SignUpImg} alt="register images" />
                     </div>
                     <div className="card w-full max-w-sm ">
-                        <form className="card-body">
+                        <form
+                            onSubmit={handleSubmit(onSubmit)}
+                            className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
                                 <input
-                                    type="email"
+                                    {...register('name', {
+                                        required: 'Name is required',
+                                        maxLength: {
+                                            value: 20,
+                                            message: 'Max length is 20',
+                                        },
+                                    })}
+                                    type="text"
                                     placeholder="Type your name"
                                     className="input input-bordered"
-                                    required
                                 />
                             </div>
+                            <p className="text-orange-500">
+                                {errors.name?.message}
+                            </p>
+
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">PhotoURL</span>
                                 </label>
                                 <input
-                                    type="email"
+                                    {...register('photoURL')}
+                                    type="text"
                                     placeholder="PhotoURL"
                                     className="input input-bordered"
-                                    required
                                 />
                             </div>
+
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
                                 <input
+                                    {...register('email', {
+                                        required: 'Email is required',
+                                        pattern: {
+                                            value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                                            message: 'Type a valid email',
+                                        },
+                                    })}
                                     type="email"
                                     placeholder="email"
                                     className="input input-bordered"
-                                    required
                                 />
                             </div>
+                            <p className="text-orange-500">
+                                {errors.email?.message}
+                            </p>
+
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
                                 <input
+                                    {...register('password', {
+                                        required: 'Password is required',
+                                        minLength: {
+                                            value: 8,
+                                            message:
+                                                'password min length must be 8 characters',
+                                        },
+                                        pattern: {
+                                            value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+                                            message:
+                                                'At least one upper case, one lower case, one digit and special characters',
+                                        },
+                                    })}
                                     type="password"
                                     placeholder="password"
                                     className="input input-bordered"
-                                    required
                                 />
                                 <label className="label">
                                     <a
@@ -65,6 +118,10 @@ const SignUp = () => {
                                     </a>
                                 </label>
                             </div>
+                            <p className="text-orange-500">
+                                {errors.password?.message}
+                            </p>
+
                             <div className="form-control">
                                 <input
                                     type="submit"
@@ -75,7 +132,9 @@ const SignUp = () => {
                             <div className="text-center">
                                 <p className="text-orange-400">
                                     Already have an account !{' '}
-                                    <Link className="text-medium text-blue-500">
+                                    <Link
+                                        to="/signin"
+                                        className="text-medium text-blue-500">
                                         Login please
                                     </Link>
                                 </p>
