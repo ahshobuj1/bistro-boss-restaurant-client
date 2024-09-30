f796506b4e1b7679b1627254705d7c3845b18ebb
A H Shobuj <ahshobuj09@gmail.com>
2024-09-30 20:08:36 +0600

    create custom shape bar chart

--- src/pages/Dashboard/Admin/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/Admin/AdminHome/AdminHome.jsx
@@ -5,6 +5,9 @@ import {FaDollarSign, FaUsers} from 'react-icons/fa';
 import {MdProductionQuantityLimits} from 'react-icons/md';
 import {LiaCarSideSolid} from 'react-icons/lia';
 
+import {BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid} from 'recharts';
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
 const AdminHome = () => {
     const {user} = useAuth();
     const axiosSecure = useAxiosSecure();
@@ -16,6 +19,35 @@ const AdminHome = () => {
             return res.data;
         },
     });
+
+    const {data: orderData = []} = useQuery({
+        queryKey: ['admin-order', user.email],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/order-stats');
+            return res.data;
+        },
+    });
+
+    // Custom Shape Bar Chart
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${
+            x + width / 2
+        },${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${
+            y + height
+        } ${x + width}, ${y + height}
+        Z`;
+    };
+
+    const TriangleBar = (props) => {
+        // eslint-disable-next-line react/prop-types
+        const {fill, x, y, width, height} = props;
+
+        return (
+            <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />
+        );
+    };
 
     return (
         <div className="bg-slate-100 p-3">
@@ -77,6 +109,41 @@ const AdminHome = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="py-6">
+                <div className="w-1/2">
+                    {/*    <h2 className="text-2xl text-center mb-5">
+                        Custom Shape Bar for sold Category
+                    </h2> */}
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={orderData}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="category" />
+                        <YAxis />
+                        <Bar
+                            dataKey="count"
+                            fill="#8884d8"
+                            shape={<TriangleBar />}
+                            label={{position: 'top'}}>
+                            {orderData.map((entry, index) => (
+                                <Cell
+                                    key={`cell-${index}`}
+                                    fill={colors[index % 6]}
+                                />
+                            ))}
+                        </Bar>
+                    </BarChart>
+                </div>
+                <div className="w-1/2"></div>
             </div>
         </div>
     );
